43be0863512edeb939e44ea84396bc7732e1fe34
Christian Siber <siber.christian@gmail.com>
2022-06-12 22:52:33 +0200

    🧚 content(map): adapt target location

--- src/game/wizarding-world.ts
+++ src/game/wizarding-world.ts
@@ -5,7 +5,7 @@ import { useTargetMarker } from "@/leaflet/target-marker";
 import { useWizardingPraterLayer } from "@/leaflet/wizarding-prater-layer";
 import type { Location } from "@/localization/location";
 
-const TARGET_LOCATION: Location = { lat: 48.21057748283953, long: 16.40278995567293 }
+const TARGET_LOCATION: Location = { lat: 48.210651569429906, long: 16.40287623807812 }
 
 export function useWizardingWorld(element: HTMLDivElement) {
   const map = useLeaflet(element);
